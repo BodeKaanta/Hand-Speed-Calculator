8f6365316bc00416554026208a86e065df4714f0
Bode Kaanta <bodekaanta@gmail.com>
2026-01-12 13:51:14 -0700

    created method in calculator to calculate value

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator {
-    public double calculateHandSpeed(RhythmType rhythm1, RhythmType rhythm2){
-        return 0.0;
+    public double calculateHandSpeed(RhythmType rhythm1, RhythmType rhythm2, double initalBpm){
+        double newBPM;
+        double desiredRhythm = rhythm1.getDuration();
+        double actualRhythm = rhythm2.getDuration();
+
+        newBPM = initalBpm * (actualRhythm / desiredRhythm);
+
+        return newBPM;
     }
 }
