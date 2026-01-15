ec9701f74317be46d87ee0c8a4717a51c9620af2
Bode Kaanta <bodekaanta@gmail.com>
2026-01-15 02:13:38 -0700

    got rid of red banner

--- flutter/hand_speed_calculator/lib/main.dart
+++ flutter/hand_speed_calculator/lib/main.dart
@@ -11,6 +11,7 @@ class HandSpeedCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HandSpeedCalculatorPage(),
     );
   }
